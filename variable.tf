324b4a36ee4e8806f0f7d5a038bdb9cf88d4db69
Roy Sam <roy.sam@live.com>
2022-11-25 01:30:09 +0800

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  default = "AKIAWKFKAPQOOIIS2MVM"
+}
+
+variable "aws_secret_key" {
+  default = "ICXaCVBNxu1h/lcF+bkn4Tvx6ZdFg3KIP9HyR4rq"
+}
+
+variable "region" {
+  default = "ap-southeast-1"
+}
