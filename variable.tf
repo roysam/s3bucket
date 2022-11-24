25b28962ce23e6aab8891ba330e9cd5fa7106423
Roy Sam <roy.sam@live.com>
2022-11-25 01:36:22 +0800

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-  default = "AKIAWKFKAPQOOIIS2MVM"
+  default = "<INSERT_ACCESS_KEY>"
 }
 
 variable "aws_secret_key" {
-  default = "ICXaCVBNxu1h/lcF+bkn4Tvx6ZdFg3KIP9HyR4rq"
+  default = "<INSERT_SECRET_KEY>"
 }
 
 variable "region" {
