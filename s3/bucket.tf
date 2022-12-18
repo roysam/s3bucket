804912258d1c99bb5f575198f6a9d8e650a5d541
Roy Sam <roy.sam@live.com>
2022-12-18 23:55:58 +0800

    Update bucket.tf

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "mybucket" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encryption" {
-  bucket = aws_s3_bucket.bucket.id
+  bucket = aws_s3_bucket.mybucket.id
 
   rule {
     apply_server_side_encryption_by_default {
