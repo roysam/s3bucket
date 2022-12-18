a5a2d2f018cce9bdf6c8c8fa95192c5dbca1ae78
Roy Sam <roy.sam@live.com>
2022-12-19 00:06:06 +0800

    Update bucket.tf

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -3,6 +3,12 @@ resource "aws_s3_bucket" "mybucket" {
     acl = "${var.acl_value}"   
 }
 
+resource "aws_kms_key" "key" {
+  description             = var.kms_key_description
+  tags                    = var.tags
+  deletion_window_in_days = var.kms_deletion_window_in_days
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encryption" {
   bucket = aws_s3_bucket.mybucket.id
 
