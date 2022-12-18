c312aa431248de14f3318538d9c6d8099058e51a
Roy Sam <roy.sam@live.com>
2022-12-18 23:54:54 +0800

    Update bucket.tf

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -2,3 +2,14 @@ resource "aws_s3_bucket" "mybucket" {
     bucket = "${var.bucket_name}" 
     acl = "${var.acl_value}"   
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encryption" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.key.key_id
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
