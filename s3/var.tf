9dcfb2d8e46eb1b4d5c17a05c59f31ccbf3b7b39
Roy Sam <roy.sam@live.com>
2022-12-19 00:06:39 +0800

    Update var.tf

--- s3/var.tf
+++ s3/var.tf
@@ -4,3 +4,21 @@ variable "acl_value" {
     # default = "public-read-write"
     default = "private"
 }
+
+variable "kms_key_description" {
+  type    = string
+  default = "rsam-s3key"
+}
+
+variable "kms_deletion_window_in_days" {
+  type    = number
+  default = 7
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "Purpose"    = "rsam-Demo",
+    "CostCenter" = "rsam-infra"
+  }
+}
